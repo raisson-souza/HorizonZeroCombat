3d91992398292029a98a1e969a09e8bfc423f4de
Raisson Souza <raissonrai@gmail.com>
2023-04-30 15:11:27 -0300

    Smart Assets Import - Index with importations created.

---
+++ src/assets/index.js
@@ -0,0 +1,71 @@
+// AUDIOS
+import AloysThemeAudio from "./audios/aloys_theme.mp3"
+import CancelledActionAudio from "./audios/cancelled_action.wav"
+import EnergyBoostAudio from "./audios/energy_boost.wav"
+import GameStartAudio from "./audios/game_start.mp3"
+import GlitchAudio from "./audios/glitch.wav"
+import LevelUpAudio from "./audios/level_up.wav"
+import MachineDeathHitAudio from "./audios/machine_death_hit.wav"
+import MachineDeath1Audio from "./audios/machine_death.wav"
+import MachineDeath2Audio from "./audios/machine_death2.wav"
+import MachineWound1Audio from "./audios/machine_wound.wav"
+import MachineWound2Audio from "./audios/machine_wound2.wav"
+import MachineWound3Audio from "./audios/machine_wound3.mp3"
+
+// GIFS
+import AloyGif from "./gifs/aloy.gif"
+import HorizonZeroDawnGif from "./gifs/horizon_zero_dawn.gif"
+import MachineAttacking1Gif from "./gifs/machine_attacking.gif"
+import MachineAttacking2Gif from "./gifs/machine_attacking2.gif"
+import MachineAttacking3Gif from "./gifs/machine_attacking3.gif"
+import MachineWalking1Gif from "./gifs/machine_walking.gif"
+import MachineWalking2Gif from "./gifs/machine_walking2.gif"
+
+// ICONS
+
+// IMAGES
+import GrazerImage from "./images/machines/grazer.webp"
+import ScrapperImage from "./images/machines/scrapper.webp"
+import StriderImage from "./images/machines/strider.webp"
+import WatcherImage from "./images/machines/watcher.webp"
+
+import GrazerBackgroundImage from "./images/machines_background/grazer_background.webp"
+import ScrapperBackgroundImage from "./images/machines_background/scrapper_background.webp"
+import StriderBackgroundImage from "./images/machines_background/scrapper_background.webp"
+import WatcherBackgroundImage from "./images/machines_background/watcher_background.webp"
+
+import HorizonZeroDawnWallpaperImage from "./images/wallpapers/horizon-zero-dawn-wallpaper.jpg"
+
+import SwordIconImage from "./images/others/SwordIcon.png"
+
+export {
+    AloysThemeAudio,
+    CancelledActionAudio,
+    EnergyBoostAudio,
+    GameStartAudio,
+    GlitchAudio,
+    LevelUpAudio,
+    MachineDeathHitAudio,
+    MachineDeath1Audio,
+    MachineDeath2Audio,
+    MachineWound1Audio,
+    MachineWound2Audio,
+    MachineWound3Audio,
+    AloyGif,
+    HorizonZeroDawnGif,
+    MachineAttacking1Gif,
+    MachineAttacking2Gif,
+    MachineAttacking3Gif,
+    MachineWalking1Gif,
+    MachineWalking2Gif,
+    GrazerImage,
+    ScrapperImage,
+    StriderImage,
+    WatcherImage,
+    GrazerBackgroundImage,
+    ScrapperBackgroundImage,
+    StriderBackgroundImage,
+    WatcherBackgroundImage,
+    HorizonZeroDawnWallpaperImage,
+    SwordIconImage
+}
