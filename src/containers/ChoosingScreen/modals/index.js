7808e7d435f4d08ace3b8b85bf5d79d3d1abbc70
Raisson Souza <raissonrai@gmail.com>
2023-05-28 23:25:21 -0300

    Machines Habilities - Machines habities descriptions on Modal Component implemented.

--- src/containers/ChoosingScreen/modals/index.js
+++ src/containers/ChoosingScreen/modals/index.js
@@ -3,7 +3,13 @@ const Modals = [
     [
         "LOCK BIOFUEL",
         "modal-1",
-        <p></p>,
+        <div style={ { color: "white" } }>
+            <p>A habilidade do Watcher é o LOCK BIOFUEL.</p>
+            <p>Custa: 5🧪 Desfere: 6🗡️</p>
+            <p>LOCK BIOFUEL bloqueia o uso do Bio Combustível do oponente na sua próxima rodada.</p>
+            <br></br>
+            <p><i>LOCK BIOFUEL possui 25% de chance de ocorrer.</i></p>
+        </div>,
         '50vh', '50vw',
         'darkblue', 'blue', 18, 'white',
         true, true
@@ -11,7 +17,13 @@ const Modals = [
     [
         "STUN",
         "modal-2",
-        <p></p>,
+        <div style={ { color: "white" } }>
+            <p>A habilidade do Strider é o STUN.</p>
+            <p>Custa: 5🧪 Desfere: 5🗡️</p>
+            <p>STUN impede que o oponente faça qualquer ação na sua próxima rodada.</p>
+            <br></br>
+            <p><i>STUN possui 25% de chance de ocorrer.</i></p>
+        </div>,
         '50vh', '50vw',
         'darkblue', 'blue', 18, 'white',
         true, true
@@ -19,7 +31,13 @@ const Modals = [
     [
         "DAMAGE BIOFUEL",
         "modal-3",
-        <p></p>,
+        <div style={ { color: "white" } }>
+            <p>A habilidade do Scrapper é o DAMAGE BIOFUEL.</p>
+            <p>Custa: 5🧪 Desfere: 6🗡️</p>
+            <p>DAMAGE BIOFUEL danifica o container de Bio Combustível do oponente, fazendo-o perder 5 pontos de energia.</p>
+            <br></br>
+            <p><i>DAMAGE BIOFUEL possui 25% de chance de ocorrer.</i></p>
+        </div>,
         '50vh', '50vw',
         'darkblue', 'blue', 18, 'white',
         true, true
@@ -27,7 +45,13 @@ const Modals = [
     [
         "DISABLE ATTACK",
         "modal-4",
-        <p></p>,
+        <div style={ { color: "white" } }>
+            <p>A habilidade do Grazer é o DISABLE ATTACK.</p>
+            <p>Custa: 5🧪 Desfere: 5🗡️</p>
+            <p>DISABLE ATTACK impede que o oponente realize um ataque normal na próxima rodada.</p>
+            <br></br>
+            <p><i>DISABLE ATTACK possui 25% de chance de ocorrer.</i></p>
+        </div>,
         '50vh', '50vw',
         'darkblue', 'blue', 18, 'white',
         true, true
